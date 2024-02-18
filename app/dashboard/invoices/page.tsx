c5e7c9b902b0ad77070a222369058cd23d9355a0
kilandaherve <kilandaherve@gmail.com>
2024-02-18 16:53:04 +0100

    dashboard

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="container flex h-screen items-center justify-center">
+      <button className="rounded bg-green-500 px-4 py-2 font-bold text-white">
+        Voici ta page Invoices
+      </button>
+    </div>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="container flex h-screen items-center justify-center">
+      <button className="rounded bg-indigo-500 px-4 py-2 font-bold text-white">
+        Voici ta page Dashboard Home
+      </button>
+    </div>
+  );
+}
